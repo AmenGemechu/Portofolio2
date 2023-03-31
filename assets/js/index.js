e3cf5bd3585ebd335916a95b97674cf245b38627
AmenGedeno <amengemechu@yahoo.com>
2023-03-31 12:41:53 +0000

    Create pick aa hand & hide page function

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+//Pick a hand function
+const pickUserHand = (hand) => {
+    console.log(hand);
+    //hide the current page
+    let hands = document.querySelector(".hands");
+    hands.style.display = "none";
+
+
+    //Take to the next page
+}
