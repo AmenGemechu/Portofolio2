d39f58e7bd070945fefa3a3191c39e173ceec07f
AmenGedeno <amengemechu@yahoo.com>
2023-04-03 08:34:08 +0000

    Set pickComputerHand function & referee function

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,9 +7,12 @@ const handOptions = {
     "spock": "assets/images/spock.png"
 }
 
-//Pick a hand function
+//Score
+let SCORE = 0;
+
+//Pick a user hand function
 const pickUserHand = (hand) => {
-    console.log(hand);
+    console.log(hand); //takeout
     //hide the current page
     let hands = document.querySelector(".hands");
     hands.style.display = "none";
@@ -19,8 +22,41 @@ const pickUserHand = (hand) => {
     contest.style.display = "flex";
 
     //Sets user selection
-        document.getElementById("userPickImage").src = handOptions[hand];
-    
+    document.getElementById("userPickImage").src = handOptions[hand];
 
+    //Computer selection
+    let computerHand = pickComputerHand();
+    
+}
+//Picka computer hand function
+const pickComputerHand = (hand) => {
+    let hands = ["rock", "paper", "scissors", "lizard", "spock"]; //Array
+    let computerHand = hands[Math.floor(Math.random() * 5)];
+
+    //Set computer selection
+    document.getElementById = ("computerPickImage").src = handOptions[computerHand];
+
+    return computerHand;
+
+}
+
+//Referee winner/looser
+const referee = (userHand, computerHand) => {
+    if(userHand == "rock" && computerHand == "paper") {
+        setWinner("YOU LOST!"){
+            else if(userHand == "rock" && computerHand == "scissors") => {
+                setWinner("You Won!")
+                setScore(SCORE + 1)
+
+    }
+
+}
+
+const setWinner = (winner) => {
+    console.log(winner);
+}
+
+const setScore = (score) => {
+    console.log(score);
 }
 
